33fbb4fadfb78dc4e5109b2cb32fc1d7280e3e92
Christopher Weaver <christopher.m.weaver@outlook.com>
2021-06-29 19:22:00 -0400

    2nd Commit

--- ProgramUI/ProgramUI.cs
+++ ProgramUI/ProgramUI.cs
@@ -4,38 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WarClasses;
+using WarEnums;
+using WarRepoNameSpace;
 
 namespace ProgramUINameSpace
 {
     public class ProgramUI
     {
-        private Queue<Deck> _decks = new Queue<Deck>();
+        private readonly WarRepo _deckRepo = new WarRepo();
 
         public void Run()
         {
-            seedData();
-            Run();
+            _deckRepo.MakeDeck();
+            Player player1 = new Player("You");
+            Player player2 = new Player("Computer");
+            List<Card> playedCards = new List<Card>();
+
+            //Console.WriteLine($"{card.Rank} of {card.Suit} ({card.Color})");
+
+            while (true)
+            {
+                player1.Score = 0;
+                player2.Score = 0;
+                _deckRepo.DealCards(player1, player2);
+
+                while (player1.Deck.Cards.Count > 0)
+                {
+                    Card p1Card = player1.Deck.Cards.Peek();
+                    Card p2Card = player2.Deck.Cards.Peek();
+                    Console.WriteLine($"{player1.Name} plays a {p1Card.Rank} of {p1Card.Suit} ({p1Card.Color})");
+                    Console.WriteLine($"{player2.Name} plays a {p2Card.Rank} of {p2Card.Suit} ({p2Card.Color})");
+
+                    Player winner = _deckRepo.PlayHand(player1, player2, playedCards);
+                    if (winner == null)
+                    {
+                        Console.WriteLine("Tie");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{winner.Name} won this hand.");
+                    }
+                }
+
+                Console.WriteLine("----------------------");
+                Console.WriteLine($"The final score is {player1.Name} {player1.Score}, {player2.Name} {player2.Score}.");
+                Console.WriteLine(player1.Score > player2.Score ? "You win!" : "You lose!");
+                Console.WriteLine("Press Escape to exit or any other key to continue.");
+
+                ConsoleKey input = Console.ReadKey().Key;
+                if (input == ConsoleKey.Escape)
+                {
+                    break;
+                }
+                Console.Clear();
+
+                _deckRepo.AddCards(_deckRepo.ShuffleDeck(playedCards));
+            }
         }
-
-        
-        public void seedData()
-        {
-            Card card1 = new Card(WarEnums.Rank.one, WarEnums.Suit.clubs, WarEnums.Color.black);
-            Card card2 = new Card(WarEnums.Rank.two, WarEnums.Suit.diamonds, WarEnums.Color.red);
-
-            Card card3 = new Card(WarEnums.Rank.three, WarEnums.Suit.spades, WarEnums.Color.black);
-            Card card4 = new Card(WarEnums.Rank.ace, WarEnums.Suit.hearts, WarEnums.Color.black);
-
-            Queue<Card> myQueue = new Queue<Card>();
-            myQueue.Enqueue(card1);
-            myQueue.Enqueue(card2);
-            myQueue.Enqueue(card3);
-            myQueue.Enqueue(card4);
-
-            Deck deck = new Deck(myQueue);
-            
-
-        }
-        }
+    }
 }
 
---
+++ WarClasses/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarClasses
+{
+    public class Player
+    {
+        //public Deck Deck { get; set; }
+        public Deck Deck = new Deck();
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public Player()
+        {
+
+        }
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public Player(string name, Deck deck)
+        {
+            Name = name;
+            Deck = deck;
+        }
+    }
+}
